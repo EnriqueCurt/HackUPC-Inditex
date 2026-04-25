4bc4ec113dbb57381b96459411ce0d5a51c7b0ad
Enrique Curt Moscoso <enrique.curtmoscoso@alum.uca.es>
2026-04-26 00:59:58 +0200

    prueba V2

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+import sys
+
+
+if __name__ == "__main__":
+    port = os.getenv("PORT", "8501")
+    cmd = [
+        sys.executable,
+        "-m",
+        "streamlit",
+        "run",
+        "app_streamlit.py",
+        "--server.address",
+        "0.0.0.0",
+        "--server.port",
+        port,
+    ]
+    raise SystemExit(subprocess.call(cmd))
